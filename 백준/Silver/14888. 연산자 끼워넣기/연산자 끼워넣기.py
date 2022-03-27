a707809bbfd4e4d97b438389294cec5c553a4c7a
3jieun3 <97589856+3jieun3@users.noreply.github.com>
2022-03-28 01:14:30 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 96 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,8 +1,8 @@
-# 가지치기 어떻게?
+# DFS
 import sys
 input = sys.stdin.readline
 
-def calcul_dfs(i, n, rst):
+def calcul_dfs(i, n, rst, op):
 	global min_rst, max_rst
 	if i == n:
 		if rst > max_rst:
@@ -11,29 +11,29 @@ def calcul_dfs(i, n, rst):
 			min_rst = rst
 		return
 
-	for j in range(i, n):
-		op_arr[i], op_arr[j] = op_arr[j], op_arr[i]
-		if op_arr[i] == 0:
-			calcul_dfs(i + 1, n, rst + Array[i])
-		elif op_arr[i] == 1:
-			calcul_dfs(i + 1, n, rst - Array[i])
-		elif op_arr[i] == 2:
-			calcul_dfs(i + 1, n, rst * Array[i])
-		else:
-			calcul_dfs(i + 1, n, int(rst / Array[i]))
-		op_arr[i], op_arr[j] = op_arr[j], op_arr[i]
+	if op[0]:
+		op[0] -= 1
+		calcul_dfs(i + 1, n, rst + A[i], op)
+		op[0] += 1
+	if op[1]:
+		op[1] -= 1
+		calcul_dfs(i + 1, n, rst - A[i], op)
+		op[1] += 1
+	if op[2]:
+		op[2] -= 1
+		calcul_dfs(i + 1, n, rst * A[i], op)
+		op[2] += 1
+	if op[3]:
+		op[3] -= 1
+		calcul_dfs(i + 1, n, int(rst / A[i]), op)
+		op[3] += 1
 
 
 N = int(input())                            # 수 개수 2 ≤ N ≤ 11
-Array = list(map(int, input().split()))     # 1 ≤ 수열 ≤ 100
-int_arr = list(map(int, input().split()))   # +, -, x, % 개수
+A = list(map(int, input().split()))         # 1 ≤ 수열 ≤ 100
+op_arr = list(map(int, input().split()))    # +, -, x, % 개수
 
-op_arr = [0]                                # 연산자 배열 ([1] ~ [N - 1])
-for o in range(4):
-	for _ in range(int_arr[o]):
-		op_arr.append(o)
-
-min_rst, max_rst = 10**9, -10**9             # 최솟값, 최댓값
-calcul_dfs(1, N, Array[0])
+min_rst, max_rst = 10**9, -10**9              # 최솟값, 최댓값
+calcul_dfs(1, N, A[0], op_arr)
 print(max_rst)
 print(min_rst)
