3a8bfad0a973170e8fc158798d3cc8a64594d839
3jieun3 <97589856+3jieun3@users.noreply.github.com>
2022-05-31 19:32:19 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 88 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,39 +1,30 @@
-# DFS
 import sys
 input = sys.stdin.readline
 
-def calcul_dfs(i, n, rst, op):
-	global min_rst, max_rst
-	if i == n:
-		if rst > max_rst:
-			max_rst = rst
-		if rst < min_rst:
-			min_rst = rst
+def recur(i, rst, add, sub, mul, div):
+	global mn, mx
+	if i == N:
+		if mn > rst:
+			mn = rst
+		if mx < rst:
+			mx = rst
 		return
 
-	if op[0]:
-		op[0] -= 1
-		calcul_dfs(i + 1, n, rst + A[i], op)
-		op[0] += 1
-	if op[1]:
-		op[1] -= 1
-		calcul_dfs(i + 1, n, rst - A[i], op)
-		op[1] += 1
-	if op[2]:
-		op[2] -= 1
-		calcul_dfs(i + 1, n, rst * A[i], op)
-		op[2] += 1
-	if op[3]:
-		op[3] -= 1
-		calcul_dfs(i + 1, n, int(rst / A[i]), op)
-		op[3] += 1
+	if add:
+		recur(i + 1, rst + A[i], add - 1, sub, mul, div)
+	if sub:
+		recur(i + 1, rst - A[i], add, sub - 1, mul, div)
+	if mul:
+		recur(i + 1, rst * A[i], add, sub, mul - 1, div)
+	if div:
+		recur(i + 1, int(rst / A[i]), add, sub, mul, div - 1)
 
 
-N = int(input())                            # 수 개수 2 ≤ N ≤ 11
-A = list(map(int, input().split()))         # 1 ≤ 수열 ≤ 100
-op_arr = list(map(int, input().split()))    # +, -, x, % 개수
 
-min_rst, max_rst = 10**9, -10**9              # 최솟값, 최댓값
-calcul_dfs(1, N, A[0], op_arr)
-print(max_rst)
-print(min_rst)
+N = int(input())
+A = list(map(int, input().split()))
+numbers = list(map(int, input().split()))   # +, -, *, %
+mn, mx = 1000000000, -1000000000
+recur(1, A[0], *numbers)
+print(mx)
+print(mn)
