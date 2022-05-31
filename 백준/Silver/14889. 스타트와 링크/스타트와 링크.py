8ee66860ced0ba0ef7d5be2662d11502d5243ddd
3jieun3 <97589856+3jieun3@users.noreply.github.com>
2022-05-31 21:21:35 +0900

    [Silver II] Title: 스타트와 링크, Time: 452 ms, Memory: 122464 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,34 +1,52 @@
 import sys
 input = sys.stdin.readline
 
-def ability_diff(start, link):                  # 능력치 합산 -> 차이 갱신
+def cal_nCr():
+	a, b, n = 1, 1, N
+	while n > N // 2:
+		a *= n
+		b *= (N + 1 - n)
+		n -= 1
+	return a // b
+
+def cal_stats(start, link):                     # 능력치 합산 -> 차이 갱신
 	global min_diff
-	s_start, s_link = 0, 0
-	for i in range(r):
-		for j in range(i + 1, r):
-			s_start += S[start[i]][start[j]] + S[start[j]][start[i]]
-			s_link += S[link[i]][link[j]] + S[link[j]][link[i]]
+	start_stat = 0
+	link_stat = 0
+	for i in range(N // 2):
+		for j in range(i + 1, N // 2):
+			start_stat += S[start[i]][start[j]] + S[start[j]][start[i]]
+			link_stat += S[link[i]][link[j]] + S[link[j]][link[i]]
 
-	min_diff = min(min_diff, abs(s_start - s_link))        # 최소 차이 갱신
+	if min_diff > abs(start_stat - link_stat):      # 최소 차이 갱신
+		min_diff = abs(start_stat - link_stat)
 
 
-def combination(i, cnt, combi):
+def combi(i, r, start, link):
+	global case_cnt
 
-	if cnt == r:                                # basis part
-		toggle = [x for x in range(1, n) if x not in combi]
-		ability_diff(combi, toggle)         # 능력치 계산
+	if case_cnt > combi_cnt // 2:                   # 가지치기 : nCr 의 절반 계산한 경우 종료
+		return
+	if r == N // 2:                                 # basis part
+		if len(link) < N // 2:                      # 덜 구한 link 구하기
+			for k in range(start[-1] + 1, N + 1):
+				link.append(k)
+		case_cnt += 1
+		cal_stats(start, link)
 		return
 
-	for j in range(i, n):                       # inductive part
-		if j not in combi:                      # 이미 넣었던 i까지는 다시 넣지 않음
-			combination(j + 1, cnt + 1, combi + [j])
+	tmp = []
+	for j in range(i, N + 1):                       # inductive part
+		combi(j + 1, r + 1, start + [j], link + tmp)
+		tmp.append(j)   # start에 포함했던 숫자 누적하여 link로
 
 
 N = int(input())
 S = [[0] * (N + 1)] + [[0] + list(map(int, input().split())) for _ in range(N)]
 
-n, r = N + 1, N // 2
-min_diff = 100 * 100
-used_combi = []
-combination(1, 0, [])
+combi_cnt = cal_nCr()   # nCr 경우의 수
+case_cnt = 0            # combi 함수로 m개 조합 구한 수
+min_diff = 999999999
+
+combi(1, 0, [], [])
 print(min_diff)
