28da9ce1c44da55ae33acdda814990be38295a90
3jieun3 <97589856+3jieun3@users.noreply.github.com>
2022-06-30 19:19:21 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 700 ms, Memory: 38100 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,33 +1,25 @@
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-# from collections import defaultdict
-def dfs(startv):
-    stack = [startv]
-    visited = []
-    while stack:
-        now = stack.pop()
-        if now not in visited:
-            visited.append(now)
-            nxt = [v for v in adj[now][::-1] if v not in visited]
-            stack = stack + nxt
-    return visited
 
-n, m = map(int, input().split())
-adj = {i:[] for i in range(1, n+1)} #= adj = defaultdict(list)
-for _ in range(m):
-    u, v = map(int, input().split())
-    adj[u].append(v)
-    adj[v].append(u)
+def dfs(n1):
+	for n2 in range(1, N + 1):
+		if adj[n1][n2] == 1 and visited[n2] == 0:
+			visited[n2] = 1
+			dfs(n2)
 
-for i in range(1, n+1):
-    adj[i].sort()
 
-cnt = 0   
-while adj:
-    cnt += 1
-    key = list(adj.keys())
-    did = dfs(key[0])
-    for v in did:
-        adj.pop(v)
-        
+N, M = map(int, input().split())
+adj = [[0] * (N + 1) for _ in range(N + 1)]
+for _ in range(M):              # 간선
+	u, v = map(int, input().split())
+	adj[u][v] = 1
+	adj[v][u] = 1
+
+cnt = 0
+visited = [0] * (N + 1)
+for i in range(1, N + 1):
+	if visited[i] == 0:         # 방문 안한 정점에서 dfs하여 연결 요소 모두 방문
+		cnt += 1
+		dfs(i)
 print(cnt)
