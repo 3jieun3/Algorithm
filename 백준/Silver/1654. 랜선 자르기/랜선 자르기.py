9708ce1c82602c162ae40420a49dfe21e0ccab65
3jieun3 <97589856+3jieun3@users.noreply.github.com>
2022-07-13 13:49:43 +0900

    [Silver II] Title: 랜선 자르기, Time: 124 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -2,19 +2,24 @@
 #1 '2' 3  4              -> 4 + 5 + 9 + 10 = 28 > 14 | (1 2 4) -> (3 2 4)
 #     '3' 4              -> 3 + 3 + 6 + 6 = 18 > 14  | (3 3 4) -> (4 3 4)
 #        '4'             -> 2 + 2 + 4 + 5 = 13 < 14  | (4 4 4) -> (4 4 3)
-k, n = map(int, input().split())
-have = [int(input()) for _ in range(k)]
-# maxlen <= min(have) 
-startlen = 1
-endlen = max(have)
-while startlen <= endlen:
-    cnt = 0
-    mid = (startlen+endlen)//2
-    for h in have:
-        cnt += h//mid
 
-    if cnt >= n: # == 더 크게 자르면 cnt 작아짐
-        startlen = mid + 1
-    else:
-        endlen = mid - 1
-print(endlen)
+import sys
+input = sys.stdin.readline
+
+K, N = map(int, input().split())
+have = [int(input()) for _ in range(K)]
+mx_rst = 0          # 만들 수 있는 최대 랜선 길이
+
+left = 1            # 랜선 길이 범위
+right = max(have)   # 랜선 길이 범위
+while left <= right:
+	cnt = 0
+	mid = (left + right) // 2   # 가운데 값 길이로 만든다면
+	for i in range(K):
+		cnt += have[i] // mid
+
+	if cnt >= N:                # N개 이상 만들어지면 더 크게 자르기
+		left = mid + 1
+	else:                       # N개 안만들어지면 더 작게 자르기
+		right = mid - 1
+print(right)
