632559d7e365615a63f7631e9569c9fe91bacb93
3jieun3 <97589856+3jieun3@users.noreply.github.com>
2022-07-24 00:56:33 +0900

    [Gold IV] Title: N-Queen, Time: 15860 ms, Memory: 156428 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,45 +1,52 @@
 def diagonal_check(r, c):
-	for i in range(N):
-		if i > 0 and 0 <= r - i < N and 0 <= c - i < N:   # 2사분면 방향 대각선에 다른 퀸 존재하는 경우
-			if board[r - i][c - i] == 1:
-				return False
-		if i > 0 and 0 <= r - i < N and 0 <= c + i < N:   # 1사분면 방향 대각선에 다른 퀸 존재하는 경우
-			if board[r - i][c + i] == 1:
-				return False
-	return True
+    for dr, dc in [(-1, -1), (-1, 1)]:
+        d = 0
+        while True:
+            d += 1
+            nr = r + dr * d
+            nc = c + dc * d
+            if 0 > nr or nr >= N or 0 > nc or nc >= N:
+                break
+            if row_flag[nr] == nc:
+                return True
+    return False
 
-def DFS_backtracking(r, c):
-	global case_cnt
-	if not diagonal_check(r, c):            # 가지치기 - 대각선에서 중복 배치 불가능
-		return
 
-	if r + 1 == N:                          # 기본 파트 - 모든 행 탐색 완료시 종료
-		case_cnt += 1
-		return
+def dfs_back(r):
+    global tot_case
 
-	for nc in range(N):                     # 유도 파트 - r + 1 행의 모든 열 탐색
-		if col_flag[nc]:                    # 해당 열에 이미 있으면 진행하지 않음
-			continue
-		if nc == c + 1 or nc == c - 1:      # (r, c)의 대각선열이면 진행하지 않음
-			continue
-		board[r + 1][nc] += 1
-		col_flag[nc] = True
-		DFS_backtracking(r + 1, nc)
-		board[r + 1][nc] = 0
-		col_flag[nc] = False
+    if r == N:
+        tot_case += 1
+        return
 
-def N_queen():
-	for j in range(N):                  # 0 행 모든 열을 순차적으로 루트로
-		board[0][j] += 1
-		col_flag[j] = True
-		DFS_backtracking(0, j)
-		board[0][j] -= 1
-		col_flag[j] = False
-	return case_cnt
+    for c in range(N):
+        # 같은 열에 퀸 있는지 체크
+        if col_flag[c]:
+            continue
+        # 대각선 상에 퀸 있는지 체크
+        if diagonal_check(r, c):
+            continue
+
+        # 가능한 경우
+        row_flag[r] = c         # 해당 행에 열 위치 저장
+        col_flag[c] = True      # 해당 열 사용 체크
+        dfs_back(r + 1)
+        row_flag[r] = 0
+        col_flag[c] = False
+
+
+def n_queen():
+    # 0 행 모든 열 한 번씩 시작
+    for c in range(N):
+        row_flag[0] = c
+        col_flag[c] = True
+        dfs_back(1)
+        col_flag[c] = False
 
 
 N = int(input())
-board = [[0] * N for _ in range(N)]
-col_flag = [False] * N
-case_cnt = 0
-print(N_queen())
+tot_case = 0
+row_flag = [0 for _ in range(N)]
+col_flag = [False for _ in range(N)]
+n_queen()
+print(tot_case)
